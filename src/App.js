27611d4a4bc6e23c0dfed22ca45b7b7490b10e00
jaejinee <012h21l@gmail.com>
2022-05-10 18:01:35 +0900

    path edit publish R2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
       </Routes>
     </Router>
   );
